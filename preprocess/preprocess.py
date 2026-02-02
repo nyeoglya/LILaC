8ff19791c3976edccd86fcd035b67a1ae976e9d6
nyeoglya <hyunseong@postech.ac.kr>
2026-02-02 08:31:59 +0000

    fix: improve code base

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,17 +1,15 @@
-import os
-
 from pathlib import Path
 
 from crawler.wiki import *
 from parser.wiki import *
 
-crawl_folder_path = "./dataset/crawl/mmqa_html/"
-img_crawl_folder_path = "./dataset/crawl/mmqa_image/"
+CRAWL_HTML_FOLDER = "/dataset/crawl/mmqa_html/"
+CRAWL_IMAGE_FOLDER = "/dataset/crawl/mmqa_image/"
 
 def main():
     # Crawler
-    wiki_batch_crawler = WikiBatchCrawler(crawl_folder_path)
-    mmqa_titles = wiki_batch_crawler.get_clean_wiki_titles("dataset/mmqa/MMQA_dev.jsonl", "dataset/mmqa/MMQA_texts.jsonl", "dataset/mmqa/MMQA_images.jsonl", "dataset/mmqa/MMQA_tables.jsonl")
+    wiki_batch_crawler = WikiBatchCrawler(CRAWL_HTML_FOLDER)
+    mmqa_titles = wiki_batch_crawler.get_clean_wiki_titles("/dataset/mmqa/MMQA_dev.jsonl", "/dataset/mmqa/MMQA_texts.jsonl", "/dataset/mmqa/MMQA_images.jsonl", "/dataset/mmqa/MMQA_tables.jsonl")
     wiki_batch_crawler.run_batch(mmqa_titles)
     
     # Parser
@@ -27,8 +25,8 @@ def main():
         i += 1
     
     # Image Crawler
-    image_batch_crawler = BatchWikiImageCrawler(img_crawl_folder_path)
-    path = Path(crawl_folder_path)
+    image_batch_crawler = BatchWikiImageCrawler(CRAWL_IMAGE_FOLDER)
+    path = Path(CRAWL_HTML_FOLDER)
     html_json_files = [str(file) for file in path.glob("*.json")]
     img_lists = image_batch_crawler.get_clean_imglinks(html_json_files)
     image_batch_crawler.run_batch(img_lists)
